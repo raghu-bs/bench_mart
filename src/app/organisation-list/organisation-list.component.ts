6fd035aa4647c9b70f503d8d2f8dc89775a8f3fa
vanitha91 <vanitha.l@tvsnext.in>
2019-04-09 10:53:33 +0530

    org list mobile and web

--- src/app/organisation-list/organisation-list.component.ts
+++ src/app/organisation-list/organisation-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-organisation-list',
@@ -7,7 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganisationListComponent implements OnInit {
 
-  constructor() { }
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.XSmall)
+    .pipe(
+      map(result => result.matches)
+    );
+  small = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver
+      .observe([Breakpoints.XSmall])
+      .subscribe((state: BreakpointState) => { console.log(state);
+        if (state.matches) {
+          this.small = true;
+          console.log(
+            'Matches small viewport or handset in portrait mode'
+          );
+        } else {
+          this.small = false
+        }
+      });
+  }
 
   ngOnInit() {
   }
